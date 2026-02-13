1fa2a1ccaf98b434ad945f08f1e0d0004ad7bf97
Ry Suriyathep <rysu986@gmail.com>
2026-02-12 23:05:02 -0800

    just kill me slowly

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,14 +1,32 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
+const getDatabaseConfig = () => {
+  const connectionString = process.env.DATABASE_URL;
+
+  if (!connectionString) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
+  const url = new URL(connectionString);
+
+  let host = url.hostname;
+
+  return {
+    user: url.username,
+    password: decodeURIComponent(url.password),
+    host: host,
+    port: parseInt(url.port) || 5432,
+    database: url.pathname.split("/")[1],
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 10000, // Increased timeout
+  };
+};
+
+const pool = new Pool(getDatabaseConfig());
 
 pool.on("connect", () => {
   console.log("Connected to Supabase PostgreSQL");
