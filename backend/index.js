266332ae82bb78f61a0d2f6d3dbd2a3b14e01f04
Ry Suriyathep <rysu986@gmail.com>
2026-02-12 22:54:17 -0800

    fixing cors

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,34 @@ const stripeRoutes = require("./routes/stripe");
 const app = express();
 
 app.use(cookieParser());
+
+const allowedOrigins = (process.env.CLIENT_URL || "http://localhost:5173")
+  .split(",")
+  .map((url) => url.trim().replace(/\/$/, ""));
+
+console.log("Allowed CORS origins:", allowedOrigins);
+console.log("CLIENT_URL from env:", process.env.CLIENT_URL);
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL?.split(",") || ["http://localhost:5173"],
+    origin: function (origin, callback) {
+      console.log("Request from origin:", origin);
+
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        console.log("Origin not allowed:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
 app.use(express.json());
 
 // Routes
